79f319a3c95402ce9501fda0d98aa14281a7b042
MBakr <moh.bakr27@gmail.com>
2022-03-10 14:55:24 +0200

    Seeds and factories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{  'Rivian' }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,8 +26,8 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+            <a class="navbar-brand" href="{{ route('/') }}">
+                {{ __('Rivian') }}
             </a>
             <a class="navbar-brand" href="{{ route('communities.index') }}">
                 {{ __('MyCommunities') }}
